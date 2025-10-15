4a9cc92fc79fb5e2a22d7c4bf067f657dbcdd46e
Monisha_Ishwar <142509375+123monisha@users.noreply.github.com>
2025-10-15 19:57:29 +0530

    Create app.module.ts

---
+++ app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { LoginComponent } from './login/login.component';
+import { RegistrationsComponent } from './registrations/registrations.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegistrationsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
+    
+  ],
+  providers: [],
+  bootstrap: [LoginComponent]
+})
+export class AppModule { }
